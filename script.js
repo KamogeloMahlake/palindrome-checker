f84de97b6ac182b8b11218af95a25d0a7337e092
KamogeloMahlake <3949843@myuwc.ac.za>
2024-10-20 12:07:54 +0200

    add new function

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ const textInput = document.querySelector("#text-input");
 const checkBtn = document.querySelector("#check-btn");
 const result = document.querySelector("#result");
 
-checkBtn.addEventListener("click", () => { if (textInput.value === "") {
+checkBtn.addEventListener("click", () => { 
+    if (textInput.value === "") {
         alert("Please input a value")
     }
-     else{   result.textContent = checkPalindrome(textInput.value);
+     else{
+        result.textContent = checkPalindrome(textInput.value);
         textInput.value = "";
-     }
+    }
 
 })
 
 function checkPalindrome(str) {
-    
-
     const newStr = str.trim().replace(/[^A-Za-z0-9]/g, "");
-
     let reverseStr = "";
+    
     for (let i = newStr.length - 1; i >= 0;i--) {
         reverseStr += newStr[i];
     }
